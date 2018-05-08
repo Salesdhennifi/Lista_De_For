1219e5eaabb98af6f85afcd53768c6a3d2c623ff
Dhennifi Sales <38558685+Salesdhennifi@users.noreply.github.com>
2018-05-08 10:20:06 -0300

    :large_blue_circle: Caixa de pontos

---
+++ Ex04.java
@@ -0,0 +1,18 @@
+public class Ex04{
+    public static void main(String[] args){
+         
+		String pontos = ("************");
+        String pontos2 =("*          *");
+                
+		for (int i = 0; i < 1; i++) {
+			System.out.println(pontos);
+        for(int j = 0; j < 5; j++){
+              System.out.println(pontos2);  
+            }
+		}
+        System.out.println(pontos);
+
+
+	}
+
+}
