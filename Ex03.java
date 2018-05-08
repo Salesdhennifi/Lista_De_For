ac960d0b2f61c64794ee2bd6bedb9baf5a02c983
Dhennifi Sales <38558685+Salesdhennifi@users.noreply.github.com>
2018-05-08 10:15:13 -0300

    :dart: Quadrado de Riscos

---
+++ Ex03.java
@@ -0,0 +1,9 @@
+public class Ex03{
+    public static void main(String[] args){
+         
+		String pontos = ("*************************");            
+		for (int i = 0; i < 10; i++) {
+			System.out.println(pontos);
+		}
+    }
+}
